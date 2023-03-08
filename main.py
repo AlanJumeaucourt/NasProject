0fb13928d246e0572b4c6da0c2911e71f90110bb
alan jumeaucourt <alan.jumeaucourt@gmail.com>
2023-03-08 19:52:14 +0100

    Add interfaces in Router object + retrieve real interface from gns3 API

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from tabulate import tabulate
 
 
 class Routeur:
-    def __init__(self, name, typeof):
+    def __init__(self, name, uid, typeof):
         self.name = name
         self.typeof = typeof
+        self.interfaces = {}
+        self.uid = uid
 
     def __str__(self):
-        return f"""
-Name: {self.name} 
+        return f"""Name: {self.name} 
+Uid : {self.uid}
 typeof: {self.typeof}
+Interfaces : {self.interfaces}
 """
 
 
@@ -21,16 +24,35 @@ if __name__ == '__main__':
     gns3_server = gns3fy.Gns3Connector("http://localhost:3080")
     print(gns3_server.projects_summary(is_print=False))
 
+    listRouteur = []
+
     lab = gns3fy.Project(name="Default.rdp", connector=gns3_server)
     lab.get()
+    print(lab.nodes[0].ports[0]['name'])
+
+    # Add object router in list with name and uid
     for node in lab.nodes:
-        print(f"Node: {node.name}")
-        for port in node.ports:
-            print(port)
-            print(f"Name: {port['name']}")
+        listRouteur.append(Routeur(node.name, node.node_id, "Pe"))
+
+    for i in range(len(listRouteur)):
+        for port in lab.nodes[i].ports:
+            listRouteur[i].interfaces[port['name']] = "Not Connected"
+
+    print(listRouteur[0])
+    # for router in listRouteur:
+    #     router.interfaces[lab.nodes.ports] = "test"
+
+    print("interface de la premiere connection : " + lab.links[0].nodes[0]['label']['text'])
+    print("interface de la deuxieme connection : " + lab.links[0].nodes[1]['label']['text'])
+    print(lab.links[0].nodes[0]['node_id'])
+    firstRouterConnected = ""
+    secondRouterConnected = ""
+    for routeur in listRouteur:
+        if routeur.uid == lab.links[0].nodes[0]['node_id']:
+            firstRouterConnected = routeur.name
+        elif routeur.uid == lab.links[0].nodes[1]['node_id']:
+            secondRouterConnected = routeur.name
+    print(firstRouterConnected + " is connected to " + secondRouterConnected)
 
 
     print('Hello World')
-    r1 = Routeur("r1", "CE")
-
-    print(r1)
