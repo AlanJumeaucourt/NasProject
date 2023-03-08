409e7c17429de9c4d78b0df64e69edb2544fabaf
alan jumeaucourt <alan.jumeaucourt@gmail.com>
2023-03-08 11:41:10 +0100

    Add Router Object and his print function

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Routeur:
+  def __init__(self, name, typeof):
+    self.name = name
+    self.typeof = typeof
+
+  def __str__(self):
+    return f"""
+Name: {self.name} 
+typeof: {self.typeof}
+"""
 
 
 # Project is to setup/automate an entire network with MPLS
+# Type of router : CE (Customer Edge), P(Provider), PE(Provider Edge)
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('Hello World')
+    r1 = Routeur("r1", "CE")
+
+    print(r1)
